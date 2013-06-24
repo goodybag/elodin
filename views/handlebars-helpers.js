a89bd644e67ac5466a4f3446417baa021faf0eda
Geoffrey Parker <grp@gparker.co>
2013-06-24 12:59:14 -0500

    forgot helpers file

---
+++ views/handlebars-helpers.js
@@ -0,0 +1,14 @@
+module.exports.register = function(Handlebars) {
+  Handlebars.registerHelper('filepicker', function(url, width, height){
+    if (typeof url !== 'string') return null;
+    var result = url.replace('www', 'cdn');
+
+    if (result.indexOf('convert') === -1)
+      result += "/convert?cache=true&fit=crop"
+
+    result += "&w=" + (width   || 100);
+    result += "&h=" + (height  || 100);
+
+    return result;
+  });
+};
